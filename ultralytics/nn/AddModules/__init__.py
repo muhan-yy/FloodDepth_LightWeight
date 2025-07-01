95cf8c6276540e166b38517702e661f4da491747
Muhan <144007019+muhan-yy@users.noreply.github.com>
2025-07-01 19:10:11 +0800

    Delete ultralytics/nn directory

--- ultralytics/nn/AddModules/__init__.py
+++
@@ -1,7 +0,0 @@
-from .DAT import *   # Deformable Attention Transformer
-from .DyC import *   # Dynamic Conv
-from .MLLA import *  # Demystify Mamba in Vision: A Linear Attention Perspective
-from .SDI import *      #  U-Netv2
-from .FreqFusion import *   # FreqFusion
-from .DySample import *
-from .MobileNetV4 import *
